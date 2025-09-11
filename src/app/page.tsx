2808437d479f9ec3db71addb743613566467a82a
Mia <74389960+founteina@users.noreply.github.com>
2025-09-11 21:12:59 +0700

    app(feat): splash screen using reactbits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
-import Image from "next/image";
+"use client";
+import React from "react";
+import BlurText from "../components/BlurText";
+import Plasma from "../components/Plasma";
 
-export default function Home() {
+const page = () => {
+  const handleAnimationComplete = () => {
+    console.log("Animation completed!");
+  };
   return (
-    <div>Day1</div>
+    <div className="flex w-full h-screen justify-center items-center relative">
+      <div>
+          <BlurText
+            text="Hi my name is Maria Founteina Liwe!"
+            delay={150}
+            animateBy="words"
+            direction="top"
+            onAnimationComplete={handleAnimationComplete}
+            className="text-[50px] font-bold text-white z-20"
+          />
+      </div>
+      <div className="absolute w-full h-screen z-10 opacity-50">
+        <Plasma
+          color="#F98B88"
+          speed={1}
+          direction="forward"
+          scale={1.1}
+          opacity={0.8}
+          mouseInteractive={false}
+        />
+      </div>
+    </div>
   );
-}
+};
+
+export default page;
